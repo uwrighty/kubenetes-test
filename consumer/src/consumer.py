b3f2becffca0022ed0e21daf7a7511ae6023fc03
Mark W <markw@loneanimal.co.uk>
2019-01-31 15:18:40 +0000

    get db, queue, kibana and producer/consumer working.

--- consumer/src/consumer.py
+++ consumer/src/consumer.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 import pika
 from elasticsearch import Elasticsearch
-
-es = Elasticsearch([{'host': 'db', 'port': 9200}])
-id = 0
-
-def callback(ch, method, properties, body):
-    # post to database
-    es.create(id=id, doc_type='_doc', index='all', body=body)
-    id = id + 1
-    print(" [x] Received %r" % body)
+import uuid
 
 
-parameters = pika.URLParameters('amqp://user:password@queue:5672/%2F?connection_attempts=100&retry_delay=5')
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue='main')
-channel.basic_consume(callback, queue='main', no_ack=True)
+class Producer:
+    def __init__(self):
+        self.es = Elasticsearch([{'host': 'db', 'port': 9200}])
+        parameters = pika.URLParameters('amqp://user:password@queue:5672/%2F?connection_attempts=100&retry_delay=5')
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='main')
+        self.channel.basic_consume(self.callback, queue='main', no_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    def callback(self, ch, method, properties, body):
+        # post to database
+        print(" [x] Received %r" % body)
+        # post to db
+        self.es.create(id=uuid.uuid4().hex, doc_type='text', index='books', body=body)
+
+
+MyProcessor = Producer()
 
 
 
--- producer/src/producer.py
+++ producer/src/producer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import pika
 from time import sleep
+import json
+
 
 parameters = pika.URLParameters('amqp://user:password@queue:5672/%2F?connection_attempts=100&retry_delay=5')
 connection = pika.BlockingConnection(parameters)
@@ -9,10 +11,14 @@ channel.queue_declare(queue='main')
 
 with open("test.txt", "r") as f:
     for line in f:
+        # create json
+        data = {}
+        data['body'] = line
+
         channel.basic_publish(exchange='',
                       routing_key='main',
-                      body=line)
-        print(" [x] Sent 'Message'")
+                      body=json.dumps(data))
+        print(" [x] Sent Message " + str(json.dumps(data)))
         sleep(1)
 
 connection.close()
